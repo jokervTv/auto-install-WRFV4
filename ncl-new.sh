ca84a7a15a11eefc44a113b4dd6e2bd5ccce71cc
jokervTv <zhangyp6603@outlook.com>
2019-02-26 18:22:30 +0800

    update

--- ncl-new.sh
+++ ncl-new.sh
@@ -6,8 +6,8 @@
 clear
 
 version_ncl="6.5.0"
-usr_libssl_ubuntu="http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4.1_amd64.deb"
-usr_libssl_debian="http://ftp.cn.debian.org/debian/pool/main/o/openssl/libssl1.1_1.1.0f-3+deb9u2_amd64.deb"
+usr_libssl_ubuntu="http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2_amd64.deb"
+usr_libssl_debian="http://mirrors.ustc.edu.cn/debian/pool/main/o/openssl/libssl1.1_1.1.0j-1~deb9u1_amd64.deb"
 contos_url="https://www.earthsystemgrid.org/dataset/ncl.650.dap/file/ncl_ncarg-6.5.0-CentOS7.5_64bit_gnu485.tar.gz"
 debian_url="https://www.earthsystemgrid.org/dataset/ncl.650.dap/file/ncl_ncarg-6.5.0-Debian9.4_64bit_gnu630.tar.gz"
 
@@ -15,7 +15,7 @@ debian_url="https://www.earthsystemgrid.org/dataset/ncl.650.dap/file/ncl_ncarg-6
 [[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] 请使用${red}管理员权限${plain}运行此脚本!" && exit 1
 
 #选择安装目录
-DEncldir="/opt/NCL"
+DEncldir="$HOME/NCL"
 echo "请手动输入 NCL 安装目录(默认配置可直接回车)："
 echo "默认安装位置(绝对路劲) $DEncldir"
 read ncldir
@@ -63,7 +63,7 @@ echo "||      安装 依赖       ||"
 echo "|||--------------------|||"
 #$sys_package update
 if [ $os_release == "centos" ]; then
-    $sys_package -y --force-yes install -y wget tcsh libbz2-dev wget xorg-dev libx11-dev gfortran x11-dev >/dev/null
+    $sys_package -y --force-yes install -y wget tcsh libbz2-dev xorg-dev libx11-dev gfortran x11-dev >/dev/null
 else
     $sys_package -y --force-yes install -y wget tcsh libxrender1 libfontconfig1 libxext6 libgfortran3 libgomp1 >/dev/null #libbz2-dev wget libX11-devel cairo-devel gcc-gfortran libxrender1
 fi
@@ -81,14 +81,14 @@ dpkg -i ./libssl1.1_1.1*
 rm ./libssl1.1_1.1*
 
 #本地环境变量修改
-if [ ! -s "~/.ncl.bashrc.bak" ]; then
-    cp ~/.bashrc ~/.ncl.bashrc.bak
-    echo "" >> ~/.bashrc
-    echo "#for NCL" >> ~/.bashrc
-    echo "export NCARG_ROOT=$ncldir" >> ~/.bashrc
-    echo 'export PATH=$NCARG_ROOT/bin:$PATH' >> ~/.bashrc
-    echo "" >> ~/.bashrc
-    source ~/.bashrc
+if [ ! -s "$HOME/.ncl.bashrc.bak" ]; then
+    cp $HOME/.bashrc $HOME/.ncl.bashrc.bak
+    echo "" >> $HOME/.bashrc
+    echo "#for NCL" >> $HOME/.bashrc
+    echo "export NCARG_ROOT=$ncldir" >> $HOME/.bashrc
+    echo 'export PATH=$NCARG_ROOT/bin:$PATH' >> $HOME/.bashrc
+    echo "" >> $HOME/.bashrc
+    source $HOME/.bashrc
 fi
 
 #NCL
@@ -108,13 +108,13 @@ else
 fi
 
 #检验安装是否成功
-source ~/.bashrc
+source $HOME/.bashrc
 version=`ncl -V`
 if [ version = "$version_ncl" ]; then
     echo -e "\n\n${green}安装成功${plain}\n\n"
     rm ncl_ncarg*
 else
     echo -e "\n\n${red}安装失败${plain}，请删除环境变量\n\n"
-    mv ~/.ncl.bashrc.bak ~/.bashrc
+    mv $HOME/.ncl.bashrc.bak $HOME/.bashrc
     echo -e "ncl安装包位于 $ncldir/"
 fi
